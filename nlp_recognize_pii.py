4aa07a3904be9d3eb2526416491ff1134c130861
EnterpriseWS <rcwang@enterprisews.net>
2019-10-20 23:48:21 -0700

    Added SSN and Personal ID regex to the function

--- nlp_recognize_pii.py
+++ nlp_recognize_pii.py
@@ -1,18 +1,6 @@
 import spacy
-from spacy.pipeline import merge_entities
-from spacy import displacy
-import spacy
-import re
-import spacy
 from spacy.matcher import Matcher
 
-# ============== SSN without hyphen ============
-# re.match(r'^(?!000|.+0{4})(?:\d{9}|\d{3}-\d{2}-\d{4})$'
-# Simplest form: ^(?!(000|666|9))\d{3}-(?!00)\d{2}-(?!0000)\d{4}$
-# With hyphen: ^(?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$
-# W/o hyphen: ^(?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}$
-# ============== member ID =====================
-# Include FEP: ^(?!999999999(\d{2}|\d)*)((([1-9]\d{8})([01]\d)*)|([rR]*\d{8}))$
 # ============== Phone number matching ==============
 # Regex: ((\(\d{3}\)?)|(\d{3}))([\s-./]?)(\d{3})([\s-./]?)(\d{4})
 # Below is training-based
@@ -30,46 +18,32 @@ from spacy.matcher import Matcher
 #     print(span.text)
 # --------------------------
 
-# ============== Regex matching ==============
+regex_ssn_hyphen = [{"TEXT": {"REGEX": "^(?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$"}}]
+regex_ssn = [{"TEXT": {"REGEX": "^(?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}$"}}]
+regex_personal_id = [{"TEXT": {"REGEX": "^(?!999999999(\d{2}|\d)*)((([1-9]\d{8})([01]\d)*)|([rR]*\d{8}))$"}}]
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("The United States of America (USA) are commonly known as the United States (U.S. or US) or America.")
-
-expression = r"[Uu](nited|\.?) ?[Ss](tates|\.?)"
-for match in re.finditer(expression, doc.text):
-    start, end = match.span()
-    span = doc.char_span(start, end)
-    # This is a Span object or None if match doesn't map to valid token sequence
-    if span is not None:
-        print("Found match:", span.text)
-
-# pattern = [{"TEXT": {"REGEX": "^[Uu](\.?|nited)$"}},
-#            {"TEXT": {"REGEX": "^[Ss](\.?|tates)$"}},
-#            {"LOWER": "president"}]
-
-# ============== Multi-rule example =============
-nlp = spacy.load("en_core_web_sm")
+matcher = Matcher(nlp.vocab)
+matcher.add("SSN_Hyphen", None, regex_ssn_hyphen)
+matcher.add("SSN", None, regex_ssn)
+matcher.add("Personal_Id", None, regex_personal_id)
 
 
-def extract_person_orgs(doc):
-    person_entities = [ent for ent in doc.ents if ent.label_ == "PERSON"]
-    for ent in person_entities:
-        head = ent.root.head
-        if head.lemma_ == "work":
-            preps = [token for token in head.children if token.dep_ == "prep"]
-            for prep in preps:
-                orgs = [token for token in prep.children if token.ent_type_ == "ORG"]
-                print({'person': ent, 'orgs': orgs, 'past': head.tag_ == "VBD"})
-    return doc
+def find_pii(text):
+    doc = nlp(text)
+    matches = matcher(doc)
+    print(text)
+    if len(matches) == 0:
+        print("No PII found")
+    else:
+        for match_id, start, end in matches:
+            string_id = nlp.vocab.strings[match_id]
+            span = doc[start:end]
+            print("Found " + string_id + ": " + span.text)
 
-
-# To make the entities easier to work with, we'll merge them into single tokens
-nlp.add_pipe(merge_entities)
-nlp.add_pipe(extract_person_orgs)
-
-doc = nlp("Alex Smith worked at Acme Corp Inc.")
-# If you're not in a Jupyter / IPython environment, use displacy.serve
-displacy.render(doc, options={'fine_grained': True})
 
 if __name__ == '__main__':
-    i = 1
-    # Do nothing yet
+    import sys
+    if len(sys.argv) == 2:
+        find_pii(sys.argv[1])
+    else:
+        print("Error: Please enter as 'py nlp_recognize_pii.py <text>'")
