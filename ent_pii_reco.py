237ada8497e8c8239cefa2ef393ef0b7d0e55352
EnterpriseWS <rcwang@enterprisews.net>
2019-10-21 22:50:37 -0700

    Added a virtual environment command file

--- ent_pii_reco.py
+++ ent_pii_reco.py
@@ -7,3 +7,5 @@ def search_pii_by_entity(doc):
             print("++ Found Name: " + ent.text)
         elif ent.label_ == 'DATE':
             print("++ Found Date: " + ent.text)
+    # TODO: Check why Spacy mis-reco 9-digit number as a DATE entity?
+    # TODO: Add a logic to remove non-date like DATE entity.
