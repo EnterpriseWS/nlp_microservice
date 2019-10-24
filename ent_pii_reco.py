48329aa020f593e36d9902d8c0ae250ac3d192ad
EnterpriseWS <rcwang@enterprisews.net>
2019-10-23 22:44:14 -0700

    Added code to display pronoun and noun as possible entities

--- ent_pii_reco.py
+++ ent_pii_reco.py
@@ -2,10 +2,16 @@ import spacy
 
 
 def search_pii_by_entity(doc):
+    # name_count = 0
     for ent in doc.ents:
         if ent.label_ == 'PERSON':
+            # name_count += 1
             print("++ Found Name: " + ent.text)
         elif ent.label_ == 'DATE':
             print("++ Found Date: " + ent.text)
     # TODO: Check why Spacy mis-reco 9-digit number as a DATE entity?
     # TODO: Add a logic to remove non-date like DATE entity.
+
+    for token in doc:
+        if token.pos_ == 'PROPN' or token.pos_ == 'NOUN':
+            print("++ Possible Name: " + token.text)
